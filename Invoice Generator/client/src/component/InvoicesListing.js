513d22a370505d11d7c641d8f2b174d807ab908e
Denish Puthawala <puthawala.denish@gmail.com>
2020-08-30 21:12:37 +0530

    ok

--- Invoice Generator/client/src/component/InvoicesListing.js
+++ Invoice Generator/client/src/component/InvoicesListing.js
@@ -1,25 +1,42 @@
 import React from 'react';
 import axios from 'axios';
 
-export default class InvoicesListing extends React.Component{
-    constructor(props){
+export default class InvoicesListing extends React.Component {
+    constructor(props) {
         super(props);
+        this.state = {
+            invoiceData: []
+        }
     }
-    componentDidMount(){
+    componentDidMount() {
         //this method runs autometically 
         axios.get('http://localhost:5000/api/readinvoice/all')
-        .then((res)=>{
-            if (res.status === 200){
-                //everything is working
-                return JSON.stringify(res);
-                //console.log('res.json()');
-            }else{
-                // something went wrong
-                console.log('problems in reading information');
-            }
-        });
-    }
-    render(){
+            .then((res) => {
+                if (res.status === 200) {
+                    //everything is working
+                    return res.data;
+                    console.log(res.data)
+                } else {
+                    // something went wrong
+                    console.log('problems in reading information');
+                }
+            }).then((resOfObj)=>{
+                let invoiceInfo = [];
+                resOfObj.map((invoice,index)=>{
+                    invoiceInfo.push({
+                        id: invoice._id,
+                        description: invoice.invoiceDescription
+                    });
+                });
+                this.setState((state,props)=>{
+                    return {
+                        invoiceData:state.invoiceData.concat(invoiceInfo)
+                    }
+                });
+                console.log(this.state.invoiceData);
+            })
+        }
+        render() {
         return (
             <h1> Hello from Invoices Listing!!!</h1>
         );
